fb055abb51e31294b24c0a4f42712d9fe719cec4
sdeagh <huw.burton@btinternet.com>
2018-04-12 18:54:44 +0100

    finishing up

--- src/App.js
+++ src/App.js
@@ -106,6 +106,7 @@ class App extends Component {
 		if (route === "signin" || route === "register") {
 			this.setState({isSignedIn: false})
 		} else if (route === 'home') {
+			this.setState({ imageUrl: '' })
 			this.setState({isSignedIn: true})
 		}
 	}
