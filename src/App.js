f108ab50c43eafcc47b8c3b15b5da8d3617b70d6
sdeagh <huw.burton@btinternet.com>
2018-04-13 11:58:08 +0100

    adding heroku server

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
 			this.state.input)
 		.then(response => {
 			if (response) {
-				fetch('http://localhost:3000/image', {
+				fetch('https://facerecognition-sdesign.herokuapp.com/image', {
 					method: 'put',
 					headers: {'Content-Type': 'application/json'},
 					body: JSON.stringify({ id: this.state.user.id })
